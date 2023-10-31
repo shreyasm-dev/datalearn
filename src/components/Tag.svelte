baffb3f91369fce7b1ad4801852c441e1936603c
shreyasm-dev <62489066+shreyasm-dev@users.noreply.github.com>
2023-10-31 16:32:48 -0500

    Update resources

---
+++ src/components/Tag.svelte
@@ -0,0 +1,12 @@
+<span
+  class="inline-flex justify-center items-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-200 rounded-full"
+>
+  <slot />
+</span>
+
+<style>
+  span {
+    background-color: #5d80fe;
+    color: white;
+  }
+</style>
--- src/routes/resources/+page.svelte
+++ src/routes/resources/+page.svelte
@@ -1,5 +1,6 @@
 <script>
   import { A, Heading } from 'flowbite-svelte';
+  import Tag from '../../components/Tag.svelte';
 </script>
 
 <Heading tag="h4">Statistics and Probability</Heading>
@@ -10,7 +11,7 @@
   href="https://www.khanacademy.org/math/statistics-probability"
   target="_blank"
 >
-  Khan Academy
+  Khan Academy - Statistics and Probability <Tag>Free</Tag>
 </A>
 
 <br />
@@ -19,7 +20,7 @@
   href="https://www.edx.org/learn/statistics/massachusetts-institute-of-technology-fundamentals-of-statistics"
   target="_blank"
 >
-  edX (MIT)
+  edX (MIT) - Fundamentals of Statistics <Tag>Free</Tag>
 </A>
 
 <br />
@@ -29,8 +30,41 @@
 
 <br />
 
-<A href="https://www.kaggle.com/learn" target="_blank">Kaggle Learn</A>
+<A href="https://www.kaggle.com/learn" target="_blank">Kaggle Learn <Tag>Free</Tag></A>
 
 <br />
 
-<A href="https://www.datacamp.com/learn/r" target="_blank">DataCamp</A>
+<A href="https://www.datacamp.com/learn/r" target="_blank">DataCamp - R</A>
+
+<br />
+<br />
+
+<Heading tag="h4">AI/ML</Heading>
+
+<br />
+
+<A
+  href="https://www.datacamp.com/courses/machine-learning-with-tree-based-models-in-r"
+  target="_blank">DataCamp - Machine Learning with Tree-Based Models in R</A
+>
+
+<br />
+
+<A
+  href="https://www.datacamp.com/courses/advanced-deep-learning-with-keras"
+  target="_blank">DataCamp - Advanced Deep Learning with Keras</A
+>
+
+<br />
+<br />
+
+<Heading tag="h4">Natural Language Processing</Heading>
+
+<br />
+
+<A
+  href="https://www.datacamp.com/courses/introduction-to-natural-language-processing-in-r"
+  target="_blank"
+>
+  DataCamp - Introduction to Natural Language Processing in R
+</A>
